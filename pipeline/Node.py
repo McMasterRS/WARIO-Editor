75e6f3810c3530cdfad2c164bfc436ab93809b82
Oliver Cook <cooko@mcmaster.ca>
2019-07-17 11:46:05 -0400

    removed obsolete classes and commented code

--- pipeline/Node.py
+++ pipeline/Node.py
@@ -11,34 +11,44 @@ class Node():
     """ Abstract Node class. This describes a single operational function in our process pipeline """
 
     ################################################################################################
+    # Node.__init__: Initializes the node
+    # + node_id: The unique id for the node.
     ################################################################################################
     def __init__(self, node_id=None):
-
+        """ Initialize Node """
         self.node_id = node_id  # identifier for the node
         self.ready = {}         # Flags for each argument, all true indicates the node should run
         self.state = {}         # Variables local to the node, set internally by itself. Persits
         self.args = {}          # Variables local to the node, set externally by it's parents
-        self.global_vars = {}       # Variables global to the entire pipeline.
+        self.global_vars = {}   # Variables global to the entire pipeline.
         self.done = True        # Flag indicating if this node requires multple passes
 
     ################################################################################################
+    # Node.start: Pipeline runs this when the pipeline starts
     ################################################################################################
     def start(self):
+        """ Default start hook ran by the pipeline before all processing begins """
         return None
         
     ################################################################################################
+    # Node.start: Pipeline runs this when the node is ready
     ################################################################################################
     def process(self):
+        """ Default process hook ran by the pipeline when this node is ready """
         return {}
 
     ################################################################################################
+    # Node.reset: Pipeline runs this to reset this nodes state variables
     ################################################################################################    
     def reset(self):
+        """ Default process hook ran by the pipeline to reset this node's state variables """
         return None
         
     ################################################################################################
+    # Node.end: Pipeline runs this when the pipeline ends
     ################################################################################################    
     def end(self):
+        """ Default end hook ran by the pipeline once the pipeline has finished """
         return None
 
     ################################################################################################
@@ -50,162 +60,3 @@ class Node():
             ready = ready and param in self.state
         print(self, self.state, self.ready, ready)
         return ready
-
-class TestImportNode(Node):
-
-    def start(self):
-        self.data = [0,1,2,3,4,5,6,7,8,9]
-        self.done = False
-
-    def process(self):
-        if len(self.data) == 0:
-            self.done = True
-            return { 'out': None}
-
-        return {
-            'out': self.data.pop()
-        }
-
-class TestAddNode(Node):
-    def process(self, A, B):
-        print(A, B)
-        return {
-            'out': A + B
-        }
-
-class TestNode(Node):
-    def process(self):
-        return {
-            'A': 0, 'B': 1
-        }
-
-###################################################################################################
-#
-###################################################################################################
-
-# class InputNode(Node):
-#     """ Node that returns part of its data each time it's run """
-
-#     batch = []  # The complete set of data for a process to run. A directory of one or more files.
-#     run = [] # A subset of a batch, a set of rows in a file for example
-#     row = [] # A subset of a run, an single row in a file for example
-#     value = None
-
-#     batch_finished = False
-#     run_finished = False
-#     row_finished = False
-#     ready = False
-
-#     def load_batch(self):
-#         return [[[]]]
-
-#     # this is part of the running code, it consumes the next piece of data, then pushes it onwards
-#     def process(self, *args, **kwargs):
-#         self.value = None
-
-#         # This is the process of iterating through and processing the batch, run, and row data
-#         if len(self.row) == 0: # this is the last one in the row
-#             self.row_finished = True
-#             if len(self.run) == 0: # this is the last one in the run
-#                 self.run_finished = True
-#                 if len(self.batch) == 0: # this is the last one in the batch
-#                     self.batch_finished = True
-#                 else: # there is still some left in the batch
-#                     self.run = self.batch.pop()
-#                     self.row = self.run.pop()
-#                     self.value = self.row.pop()
-#                     self.run_finished = False
-#                     self.row_finished = False
-#             else: # there is still som left in the run
-#                 self.row = self.run.pop()
-#                 self.value = self.row.pop()
-#                 self.row_finished = False
-#         else:
-#             self.value = self.row.pop() # we wouldnt be running this if there was nothing in the row
-
-#         # print(self.row)
-
-#         return {
-#             "out": self.value
-#         }
-
-# ###################################################################################################
-# #
-# ###################################################################################################
-
-# class OutputNode(Node):
-#     """ abstract node with no children, this is used to output the results of the entire process """
-    
-#     def finish_batch(self, batch):
-#         """ concludes the running of a batch of runs """
-    
-#     def finish_run(self, run, batch):
-#         """ concludes the running of a run """
-
-#     def output_after_batch(self, batch):
-#         return batch
-
-#     def output_after_run(self, run):
-#         return run
-
-# ###################################################################################################
-# #
-# ###################################################################################################
-
-# class FileInputNode(InputNode):
-#     """ This is an input node where each item is a row of a file or each row in a set of files. """ 
-
-#     def load_batch(self, batch=[]):
-#         """ load the data for the whole batch, in this case, a set of files """ 
-
-#         file_locations = self.prompt_file_locations()
-#         for location in file_locations:
-#             batch.append(self.read_file(location))
-
-#         return batch
-
-#     def prompt_file_locations(self):
-#         """ prompt the user with a graphical file selector """
-        
-#         file_locations = ["./test1.csv", "test2.csv"]
-
-#         return file_locations
-
-#     def read_file(self, file_location):
-#         """ reads all of the data from a set of file locations. returns the raw data """
-
-#         data = []
-
-#         with open(file_location) as f:
-#             data = list(csv.reader(f))
-
-#         return data
-
-# ###################################################################################################
-# #
-# ###################################################################################################
-
-# class RandomInputNode(InputNode):
-#     """ fills a set of columns and rows with random numbers and pushes them as a batch and runs """
-
-#     def __init__(self, n_rows=0, n_columns=0, rand_min=0, rand_max=1, precision=3):
-#         self.n_rows = n_rows
-#         self.n_columns = n_columns
-#         self.rand_min = rand_min
-#         self.rand_max = rand_max
-#         self.precision = precision
-
-#     def load_batch(self, batch=[]):
-#         batch = [[random.randrange(self.rand_min, self.rand_max) for i in range(self.n_columns)] for j in range(self.n_rows)]
-#         return batch
-
-# ###################################################################################################
-# #
-# ###################################################################################################
-# class PrintNode(Node):
-
-#     def process(self, value):
-#         print("PRINTNODE: ", value)
-#         return {
-#             'out': value
-#         }
--- pipeline/NodzInterface.py
+++ pipeline/NodzInterface.py
@@ -3,8 +3,15 @@ from pipeline.TaskFactory import NodeFactory
 
 NODZ_SAVE_LOCATION = "./pipeline/saves/sample.json"
 
+##################################################################################################
+# NodzInterface: Compartmentalizes the importing of a save file from the nodz library. Singleton
+##################################################################################################
 class NodzInterface():
 
+    ###############################################################################################
+    # Load (static): loads a well formatted json save from nodz and returns an array of nodes and
+    # the connections
+    ###############################################################################################
     @staticmethod
     def load(file_location):
 
@@ -19,23 +26,22 @@ class NodzInterface():
             if "NODES" in data:
                 for node_id in data["NODES"]:
                     node = data["NODES"][node_id]
-                    print(node['variables'])
-                    class_name = node["file"].split('.')[0]
-                    NodeFactory.import_node(node["type"], node["toolkit"], class_name)
-                    node_instance = NodeFactory.create_node(node_id, node["type"])
-                    node_instance.args = {**node['variables']}
-                    nodes.append([node_id, node_instance])
-                    nodes_mapping[node_id] = node_instance
+                    class_name = node["file"].split('.')[0] # extract the name of the class stored here as XXXX.py
+                    NodeFactory.import_node(node["type"], node["toolkit"], class_name) # uses the Nodefactory to do the import logic, this also registers it with the factory for creating new versions
+                    node_instance = NodeFactory.create_node(node_id, node["type"]) # Create a new instance of this node
+                    node_instance.args = {**node['variables']} # Copies the variables into the node as default arguments/configurations
+                    nodes.append([node_id, node_instance]) # Compact the id and instance for access later
+                    nodes_mapping[node_id] = node_instance # we use a mapping of id to instance for use parsing connections later
 
-            # TODO: kind of wierd to do the connection logic here, I should let the pipeline handle
             if "CONNECTIONS" in data:
                 for connection in data["CONNECTIONS"]:
-                    parent_id, parent_terminal = connection[0].split('.')
-                    child_id, child_terminal = connection[1].split('.')
+                    parent_id, parent_terminal = connection[0].split('.') # Stored in json as id.terminal
+                    child_id, child_terminal = connection[1].split('.') 
                     parent = nodes_mapping[parent_id]
                     child = nodes_mapping[child_id]
-                    connections.append([(parent, parent_terminal), (child, child_terminal)])
+                    connections.append([(parent, parent_terminal), (child, child_terminal)]) # stores the nodes and terminals all together
 
+            # TODO: parse the global variables contained in nodz
             if "GLOBALS" in data:
                 for global_var in data["GLOBALS"]:
                     print(global_var)
