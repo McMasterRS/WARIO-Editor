815723e0fcb5b8c220e54183c851256b22b72049
mudwayt <mudwayt@mcmaster.ca>
2019-11-29 16:47:48 -0500

    Modified pipeline import code to be able to handle scripts not in their toolkit's root directory

--- pipeline/NodeFactory.py
+++ pipeline/NodeFactory.py
@@ -1,4 +1,4 @@
-import importlib
+import importlib, os
 
 # Task Factories know about all types of tasks that can be created and creates the appropriate instance when called
 # Abstracts the creation logic, and all of the library importing away from the user
@@ -36,17 +36,14 @@ class NodeFactory():
     def import_node(cls, type_id, toolkit_id, class_name, path_toolkit_id = False):
         # If toolkit ID is a path (from the WARIO window)
         if path_toolkit_id == True: 
-            spec = importlib.util.spec_from_file_location(name = type_id, location = toolkit_id + "\\" + class_name + ".py")
+            spec = importlib.util.spec_from_file_location(name = type_id, location = os.path.normpath(toolkit_id + "\\" + class_name + ".py"))
             module = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(module)
         else:
             """ import the class for a new node and register it """
             module_name = "toolkits." + toolkit_id + '.' + class_name
             module = importlib.import_module(module_name)
-        
-        #if type_id in cls.registered_nodes:
-        #    importlib.reload(module)
 
-        cls.register_node(type_id, getattr(module, class_name))
+        cls.register_node(type_id, getattr(module, os.path.basename(class_name)))
 
         return cls.registered_nodes[type_id]
