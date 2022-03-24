a2a5d23f4c6b63ef6eda5877b85e43e00fa41708
Tom <tmudway@hotmail.co.uk>
2022-03-24 13:24:41 -0400

    Updating conf.py to include rtd theme

--- WarioEditor/docs/conf.py
+++ WarioEditor/docs/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.todo',
     'recommonmark',
     'sphinx_markdown_tables',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
